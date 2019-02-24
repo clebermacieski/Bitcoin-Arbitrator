9c0563efa71f8cc19053ca1bd316a1f889968f23
Cléber Macieski <cleber.macieski@gmail.com>
2019-02-24 19:03:03 -0300

    Adicionar arquivos de projeto.

---
+++ transacoes/Gastador.cs
@@ -0,0 +1,55 @@
+﻿using NBitcoin;
+using QBitNinja.Client;
+using System;
+
+namespace transacoes_nbitcoin
+{
+    internal class Gastador
+    {
+
+        public bool gastar(string txIdOrigem, string enderecoDestino)
+        {
+            // Private key da transação "Enviando pra mim mesmo1" que gastarei: mthg7QduM2ba3AJ5UwvQwvkuJDjLJsu9Rt
+            string chavePrivada = "cTQA9XcNUcS7CVtvAvz6BipKn5xzWTn3ppFTGCkEwe8QS9dVZPDw";
+            var bitcoinPrivateKey = new BitcoinSecret(chavePrivada);
+            var rede = bitcoinPrivateKey.Network;
+            var address = bitcoinPrivateKey.GetAddress();
+            Console.WriteLine(address);
+
+            //Consulta transação que gastarei (Um output de troco e um "Enviando pra mim mesmo1" que gastarei
+            var client = new QBitNinjaClient(rede);
+            var transactionId = uint256.Parse(txIdOrigem);
+            var transactionResponse = client.GetTransaction(transactionId).Result;
+
+            //Verifica outpoints da transação que gastarei
+            var receivedCoins = transactionResponse.ReceivedCoins;
+            OutPoint outPointToSpend = null;
+            foreach (var coin in receivedCoins)
+            {
+                Console.WriteLine(coin.TxOut.ScriptPubKey);
+                Console.WriteLine(bitcoinPrivateKey.ScriptPubKey);
+                if (coin.TxOut.ScriptPubKey == bitcoinPrivateKey.ScriptPubKey)
+                {
+                    outPointToSpend = coin.Outpoint;
+                }
+            }
+            if (outPointToSpend == null)
+                throw new Exception("TxOut doesn't contain our ScriptPubKey");
+            Console.WriteLine("We want to spend {0}. outpoint:", outPointToSpend.N + 1);
+
+            //Criando transação e adicionando o output que quero gastar no input da transação
+            var transacao= Transaction.Create(rede);
+            transacao.Inputs.Add(new TxIn()
+            {
+                PrevOut = outPointToSpend
+            });
+
+            //Destinação
+            var destino = BitcoinAddress.Create(enderecoDestino, rede);
+
+            return false;
+        }
+    }
+}
+    
+        
---
+++ transacoes/Program.cs
@@ -0,0 +1,81 @@
+﻿using NBitcoin;
+using QBitNinja.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transacoes_nbitcoin
+{
+
+    class Program
+    {
+        private static Network rede = Network.TestNet;
+
+        static void Main(string[] args)
+        {
+            //retornaInfoTransacao();
+            Gastar();
+
+        }
+
+        private static void Gastar()
+        {
+            Gastador gastador = new Gastador();
+
+            String txIdOrigem = "39f2fa57c3620ee44724244c2747d79ce6d392a54fdf637ab969911763deaa61"; // Transação de 0.01995480 enviados a mim mesmo
+
+            String enderecoDestino = "mwGYn4JXyjXXtLVsJJNDKRYS95vSG6dY3o"; //Enviado do meu programa.
+
+            gastador.gastar(txIdOrigem , enderecoDestino);
+        }
+
+
+
+
+
+
+
+
+        private static void retornaInfoTransacao()
+        {
+            void recebidos(List<ICoin> moedasRecebidas)
+            {
+                foreach (var moeda in moedasRecebidas)
+                {
+                    Money montante = (Money)moeda.Amount;
+
+                    Console.WriteLine("Montante= " + montante.ToDecimal(MoneyUnit.BTC));
+                    var scriptDePagamento2 = moeda.TxOut.ScriptPubKey;
+                    Console.WriteLine("Script de pagamento= " + scriptDePagamento2);
+                    var endereco = scriptDePagamento2.GetDestinationAddress(Network.Main);
+                    Console.WriteLine("Endereço= " + endereco);
+                    }
+            }
+
+            void consultaRede(string transacao)
+            {
+                if (transacao == null)
+                {
+                    throw new ArgumentNullException(nameof(transacao));
+                }
+
+                var cliente = new QBitNinjaClient(rede);
+
+                var transactionId = uint256.Parse(transacao);
+                var transactionResponse = cliente.GetTransaction(transactionId).Result;
+
+                Console.WriteLine(transactionResponse.TransactionId); // O próprio ID
+                Console.WriteLine(transactionResponse.Block.Confirmations); // Número de confirmações
+                recebidos(transactionResponse.ReceivedCoins);
+
+            }
+
+            String txid = "39f2fa57c3620ee44724244c2747d79ce6d392a54fdf637ab969911763deaa61";
+
+            consultaRede(txid);
+            
+        }
+    }
+}
