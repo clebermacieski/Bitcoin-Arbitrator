daddf2b92cb0b68661a8407079bc566a528936c9
Cléber Macieski <cleber.macieski@gmail.com>
2019-04-21 10:41:43 -0300

    Criação do projeto ArbitroBitcoin a partir do template MasterDetail.

---
+++ ArbitroBitcoin/ArbitroBitcoin/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ArbitroBitcoin.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ArbitroBitcoin/ArbitroBitcoin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ArbitroBitcoin.Models;
+
+namespace ArbitroBitcoin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
